d250ba6fe9a5e8b3a2fc401c51488152aac82b2b
Artjoms Figurins <92020227+Otrigos@users.noreply.github.com>
2023-06-14 18:13:54 +0200

    CI+SonarCloud

--- src/Manager/PlaceManager.ts
+++ src/Manager/PlaceManager.ts
@@ -8,30 +8,15 @@ class PlaceManager {
         this.controller = controller;
     }
     public async getAllPlaces(): Promise<PlaceDocument[]> {
-        try {
             return await this.controller.getAllPlaces();
-        } catch (err) {
-            console.error(err);
-            throw new Error('Error retrieving places');
-        }
     }
 
     public async getPlaceById(UId: string): Promise<PlaceDocument | null> {
-        try {
             return await this.controller.getPlaceById(UId);
-        } catch (err) {
-            console.error(err);
-            throw new Error('Error retrieving place');
-        }
     }
 
     public async createPlace(place: JSON, description: string): Promise<PlaceDocument> {
-        try {
             return await this.controller.createPlace(place, description);
-        } catch (err) {
-            console.error(err);
-            throw new Error('Error creating place');
-        }
     }
 }
 
